e78a49fa5a656f17931cf1e3e8a679e9e997f9aa
Bjoern Kimminich <bjoern.kimminich@gmx.de>
2016-06-30 15:19:16 +0200

    Added static import

--- botwars-java/src/main/java/de/kimminich/kata/botwars/BotFactory.java
+++ botwars-java/src/main/java/de/kimminich/kata/botwars/BotFactory.java
@@ -1,6 +1,5 @@
 package de.kimminich.kata.botwars;
 
-import de.kimminich.kata.botwars.effects.StatusEffectFactory;
 import de.kimminich.kata.botwars.effects.negative.*;
 
 import java.util.Arrays;
@@ -8,6 +7,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import static de.kimminich.kata.botwars.BotTypes.*;
+import static de.kimminich.kata.botwars.effects.StatusEffectFactory.createFactoryForEffectWithDuration;
+import static de.kimminich.kata.botwars.effects.StatusEffectFactory.createFactoryForEffectWithDurationAndAoE;
 
 final class BotFactory {
 
@@ -20,12 +21,12 @@ final class BotFactory {
 
     static Bot create(BotTypes type) {
         switch (type) {
-            case AGGRO_BOT: return new Bot(AGGRO_BOT.toString(), 100, 20, 40, 800, 0.0, 0.1, 0.1, 0.3, StatusEffectFactory.createFactoryForEffectWithDuration(1, DefenseDown.class, Stun.class));
-            case STEALTH_BOT: return new Bot(STEALTH_BOT.toString(), 70, 20, 90, 500, 0.2, 0.2, 0.0, 0.4, StatusEffectFactory.createFactoryForEffectWithDuration(2, SpeedDown.class, OffenseDown.class));
-            case GLASS_BOT: return new Bot(GLASS_BOT.toString(), 180, 0, 20, 300, 0.3, 0.1, 0.05, 0.65, StatusEffectFactory.createFactoryForEffectWithDuration(2, ContinuousDamage.class));
-            case TANK_BOT: return new Bot(TANK_BOT.toString(), 50, 40, 30, 1200, 0.05, 0.1, 0.2, 0.25, StatusEffectFactory.createFactoryForEffectWithDuration(3, Bomb.class));
+            case AGGRO_BOT: return new Bot(AGGRO_BOT.toString(), 100, 20, 40, 800, 0.0, 0.1, 0.1, 0.3, createFactoryForEffectWithDuration(1, DefenseDown.class, Stun.class));
+            case STEALTH_BOT: return new Bot(STEALTH_BOT.toString(), 70, 20, 90, 500, 0.2, 0.2, 0.0, 0.4, createFactoryForEffectWithDuration(2, SpeedDown.class, OffenseDown.class));
+            case GLASS_BOT: return new Bot(GLASS_BOT.toString(), 180, 0, 20, 300, 0.3, 0.1, 0.05, 0.65, createFactoryForEffectWithDuration(2, ContinuousDamage.class));
+            case TANK_BOT: return new Bot(TANK_BOT.toString(), 50, 40, 30, 1200, 0.05, 0.1, 0.2, 0.25, createFactoryForEffectWithDuration(3, Bomb.class));
             case BEAVERETTE_BOT: return new Bot(BEAVERETTE_BOT.toString(), 70, 30, 35, 1000, 0.05, 0.15, 0.1);
-            case KAMIKAZE_BOT: return new Bot(KAMIKAZE_BOT.toString(), 50, 0, 40, 500, 0.0, 0.2, 0.0, 0.65, StatusEffectFactory.createFactoryForEffectWithDurationAndAoE(2, Bomb.class));
+            case KAMIKAZE_BOT: return new Bot(KAMIKAZE_BOT.toString(), 50, 0, 40, 500, 0.0, 0.2, 0.0, 0.65, createFactoryForEffectWithDurationAndAoE(2, Bomb.class));
             default: throw new AssertionError("Unexpected BotType: " + type);
         }
     }
