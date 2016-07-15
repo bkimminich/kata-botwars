bc025429579811919c5230a126d7446d6ecc9f2c
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2016-07-15 10:41:24 +0200

    Tests for EffectFactory instance creation

---
+++ botwars-java/src/test/java/de/kimminich/kata/botwars/effects/EffectWithConstructor.java
@@ -0,0 +1,9 @@
+package de.kimminich.kata.botwars.effects;
+
+import de.kimminich.kata.botwars.Bot;
+
+class EffectWithConstructor extends EffectWithoutConstructor {
+    @SuppressWarnings("UnusedParameters")
+    public EffectWithConstructor(Bot invoker, Integer duration) {
+    }
+}
